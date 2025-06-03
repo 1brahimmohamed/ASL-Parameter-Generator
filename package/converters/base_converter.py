7b750d97ffea945569bc47e02b5858d8ad2e30cc
Ibrahim Mohamed <imimazim7@gmail.com>
2025-06-03 11:44:03 +0200

    chore(package):add converters and readers

--- package/converters/base_converter.py
+++ package/converters/base_converter.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class BaseConverter(ABC):
     @abstractmethod
-    def convert(self, input_path: str, output_path: str):
+    def convert(self):
         """
         Convert from one format to another.
         Must be implemented by all converters.
