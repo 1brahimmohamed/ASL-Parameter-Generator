4259838dd9308a47573e2889063867dcbfb43ad2
Ibrahim Mohamed <imimazim7@gmail.com>
2025-06-17 07:35:00 +0200

    chore: initiate get report api

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
 from .routers import report_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-app.include_router(report_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    # Origins that are allowed to make requests
+    allow_origins=["*"],  # Allow all origins (you can specify specific origins like ["http://localhost:3000"])
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+
+app.include_router(report_router, prefix="/api")
 
 @app.get("/")
 def read_root():
+
     return {
         "name": "ASL Methods Parameter Generator API Service",
         "version": "0.0.1",
@@ -13,12 +26,18 @@ def read_root():
                        "It is designed to be used in conjunction with the ASL Methods Parameter Generator frontend application.",
         "organization": "The ISMRM Open Science Initiative for Perfusion Imaging",
         "authors": [
-            "Ibrahim Abdelazim",
-            "Hanliang Xu"
+            {"Ibrahim Abdelazim": "ibrahim.abdelazim@fau.de" },
+            {"Hanliang Xu": "hxu110@jh.edu"}
         ],
         "supervisors": [
             "Jan Petr",
             "David Thomas",
         ],
+        "Specs": {
+            "Programming Language": "Python",
+            "Framework": "FastAPI",
+            "Operating System": "OS Independent",
+        },
         "license": "MIT",
     }
+
--- apps/backend/app/routers/reports.py
+++ apps/backend/app/routers/reports.py
@@ -1,10 +1,15 @@
+import os
 from fastapi import APIRouter, HTTPException, status, File, Form, UploadFile
 from typing import List, Optional
+from .data import data
 
-report_router = APIRouter()
+report_router = APIRouter(prefix="/report")
 
+# create the uploads directory if it doesn't exist
+os.makedirs("uploads", exist_ok=True)
+os.makedirs("uploads/dicom", exist_ok=True)
 
-@report_router.post("/report", response_model=dict, status_code=status.HTTP_200_OK)
+@report_router.post("/process", response_model=dict, status_code=status.HTTP_200_OK)
 async def get_report(
         files: Optional[List[UploadFile]] = File(None),
         nifti_file: Optional[UploadFile] = File(None),
@@ -14,9 +19,32 @@ async def get_report(
     Receives form data and two files: a NIfTI file and a DICOM file.
     """
 
-    return {
-        "message": "Files received",
-        "files": [f.filename for f in files] if files else [],
+    # save the incoming files to the uploads directory
+    if files:
+        for file in files:
+            await save_upload(file)
+
+    if nifti_file:
+        await save_upload(nifti_file)
+
+    if dcm_files:
+        for file in dcm_files:
+            await save_upload(file, base_dir="uploads/dicom")
+
+    print({
+        "modality": "asl",
+        "files": [file.filename for file in files ] if files else [],
         "nifti_file": nifti_file.filename if nifti_file else None,
-        "dcm_files": [d.filename for d in dcm_files] if dcm_files else [],
-    }
+        "dcm_files": [ file.filename for file in dcm_files ] if dcm_files else []
+    })
+
+    return data
+
+
+async def save_upload(upload: UploadFile, base_dir="uploads"):
+    # Use the filename as a relative path if needed
+    file_path = os.path.join(base_dir, upload.filename)
+    dir_path = os.path.dirname(file_path)
+    os.makedirs(dir_path, exist_ok=True)
+    with open(file_path, "wb") as f:
+        f.write(await upload.read())
